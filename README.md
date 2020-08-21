d86d7a2dc316f2a26dc4c92be4fd8a4b2bab9e22
Kerry Bartels <kerrybartels@Kerrys-Mac.fritz.box>
2020-08-21 21:30:16 +0200

    commit

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Portfolio Website
 
-This is the full repository of my portfolio website build with [react](https://reactjs.org/) and [p5.js](https://p5js.org/). Feel free to
+This is the repository for my portfolio website build with [react](https://reactjs.org/) and [p5.js](https://p5js.org/). Feel free to explore and edit.
+
+##
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React from 'react';
 import P5Wrapper from 'react-p5-wrapper';
 import './App.scss';
 import sketch from './sketches/sketch';
+import { ReactComponent as Logo } from './img/logo.svg';
+
+function LogoSVG() {
+  return (
+    <div className='logo'>
+      <Logo />
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'></header>
       <P5Wrapper sketch={sketch} />
+      <LogoSVG />
       <div className='title'>
-        <h1>hey, hallo</h1>
+        <h1>hallo</h1>
       </div>
     </div>
   );
